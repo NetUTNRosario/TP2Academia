e14dcb9a683236c1c4b946ffbc6be766b33291be
MartinLambrecht <69395872+MartinLambrecht@users.noreply.github.com>
2022-05-19 21:05:04 -0300

    Creación de la Logica de la Clase Usuario

---
+++ Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Database;
+using Business.Entities;
+
+namespace Business.Logic
+{
+    public class UsuarioLogic:BusinessLogic
+    {
+        private UsuarioAdapter UsuarioData;
+
+        public UsuarioLogic() { 
+        UsuarioData = new UsuarioAdapter();
+        }
+
+        public Usuario GetOne(int id) {
+            return UsuarioData.GetOne(id);
+        }
+
+        public List<Usuario> GetAll()
+        {
+            return UsuarioData.GetAll();
+        }
+
+        public void Delete (int id)
+        {
+            UsuarioData.Delete(id);
+        }
+
+        public void Save (Usuario usuario) {
+            UsuarioData.Save(usuario);
+        }
+    }
+}
