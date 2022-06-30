2af03a3ae98ab154a1738a6166c03ecc4a2607ce
utnnet2019@outlook.com <utnnet2019@outlook.com>
2022-06-30 10:36:24 -0300

    Creacion y configuracion inicial del Menu Principal

---
+++ UI.Desktop/MenuPrincipal.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace UI.Desktop
+{
+    partial class MenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuPrincipal));
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbABM = new System.Windows.Forms.ToolStripDropDownButton();
+            this.usuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.especialidadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblBienvenida = new System.Windows.Forms.Label();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.tsbOpcionesUsuario = new System.Windows.Forms.ToolStripDropDownButton();
+            this.stbModificarUsuarioActual = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarSesionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(792, 0);
+            this.toolStripContainer1.Location = new System.Drawing.Point(4, 4);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(792, 24);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbABM});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(58, 25);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // tsbABM
+            // 
+            this.tsbABM.BackColor = System.Drawing.SystemColors.Control;
+            this.tsbABM.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbABM.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuariosToolStripMenuItem,
+            this.especialidadesToolStripMenuItem});
+            this.tsbABM.Image = ((System.Drawing.Image)(resources.GetObject("tsbABM.Image")));
+            this.tsbABM.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbABM.Name = "tsbABM";
+            this.tsbABM.Size = new System.Drawing.Size(46, 22);
+            this.tsbABM.Text = "ABM";
+            // 
+            // usuariosToolStripMenuItem
+            // 
+            this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
+            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.usuariosToolStripMenuItem.Text = "Usuarios";
+            this.usuariosToolStripMenuItem.Click += new System.EventHandler(this.usuariosToolStripMenuItem_Click);
+            // 
+            // especialidadesToolStripMenuItem
+            // 
+            this.especialidadesToolStripMenuItem.Name = "especialidadesToolStripMenuItem";
+            this.especialidadesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.especialidadesToolStripMenuItem.Text = "Especialidades";
+            this.especialidadesToolStripMenuItem.Click += new System.EventHandler(this.especialidadesToolStripMenuItem_Click);
+            // 
+            // lblBienvenida
+            // 
+            this.lblBienvenida.AutoSize = true;
+            this.lblBienvenida.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.lblBienvenida.Location = new System.Drawing.Point(174, 128);
+            this.lblBienvenida.Name = "lblBienvenida";
+            this.lblBienvenida.Size = new System.Drawing.Size(466, 29);
+            this.lblBienvenida.TabIndex = 1;
+            this.lblBienvenida.Text = "Bienvenidos al sistema de La Academia";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbOpcionesUsuario});
+            this.toolStrip2.Location = new System.Drawing.Point(680, 3);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.toolStrip2.Size = new System.Drawing.Size(156, 25);
+            this.toolStrip2.TabIndex = 1;
+            // 
+            // tsbOpcionesUsuario
+            // 
+            this.tsbOpcionesUsuario.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.tsbOpcionesUsuario.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbOpcionesUsuario.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stbModificarUsuarioActual,
+            this.cerrarSesionToolStripMenuItem});
+            this.tsbOpcionesUsuario.Image = ((System.Drawing.Image)(resources.GetObject("tsbOpcionesUsuario.Image")));
+            this.tsbOpcionesUsuario.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbOpcionesUsuario.Name = "tsbOpcionesUsuario";
+            this.tsbOpcionesUsuario.Size = new System.Drawing.Size(113, 22);
+            this.tsbOpcionesUsuario.Text = "Opciones Usuario";
+            // 
+            // stbModificarUsuarioActual
+            // 
+            this.stbModificarUsuarioActual.Name = "stbModificarUsuarioActual";
+            this.stbModificarUsuarioActual.Size = new System.Drawing.Size(180, 22);
+            this.stbModificarUsuarioActual.Text = "Modificar Usuario";
+            this.stbModificarUsuarioActual.Click += new System.EventHandler(this.cambiarContraseñaToolStripMenuItem_Click);
+            // 
+            // cerrarSesionToolStripMenuItem
+            // 
+            this.cerrarSesionToolStripMenuItem.Name = "cerrarSesionToolStripMenuItem";
+            this.cerrarSesionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cerrarSesionToolStripMenuItem.Text = "Cerrar Sesion";
+            this.cerrarSesionToolStripMenuItem.Click += new System.EventHandler(this.cerrarSesionToolStripMenuItem_Click);
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBienvenida);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "MenuPrincipal";
+            this.Text = "Menu Principal";
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton tsbABM;
+        private System.Windows.Forms.ToolStripMenuItem usuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem especialidadesToolStripMenuItem;
+        private System.Windows.Forms.Label lblBienvenida;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripDropDownButton tsbOpcionesUsuario;
+        private System.Windows.Forms.ToolStripMenuItem stbModificarUsuarioActual;
+        private System.Windows.Forms.ToolStripMenuItem cerrarSesionToolStripMenuItem;
+    }
+}
---
+++ UI.Desktop/MenuPrincipal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UI.Desktop
+{
+    public partial class MenuPrincipal : Form
+    {
+        Business.Entities.Usuario _usuarioLogueado;
+
+        public Business.Entities.Usuario usuarioLogueado {
+            get { return _usuarioLogueado; }
+            set { _usuarioLogueado = value; }
+        }
+
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+        public MenuPrincipal(Business.Entities.Usuario usuarioActual) : this()
+        {
+            usuarioLogueado = usuarioActual;
+        }
+
+        private void especialidadesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Especialidades().Show();
+        }
+
+        private void usuariosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Usuarios().Show();
+        }
+
+        private void cerrarSesionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //new Logueo().Show();
+            this.Close();
+        }
+
+        private void cambiarContraseñaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //TODO: Inicio de sesion que inicialice usuarioLogueado
+            UsuarioDesktop usuarioDesk = new UsuarioDesktop(usuarioLogueado.ID, ApplicationForm.ModoForm.Modificacion);
+            usuarioDesk.ShowDialog();
+        }
+    }
+}
