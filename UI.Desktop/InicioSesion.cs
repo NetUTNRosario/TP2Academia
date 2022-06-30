3a6f3ac5a0c7747597b48a4c31bf6723990c0c91
utnnet2019@outlook.com <utnnet2019@outlook.com>
2022-06-30 11:21:13 -0300

    Creacion Inicio Sesion - Desktop

---
+++ UI.Desktop/InicioSesion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Desktop
+{
+    public partial class InicioSesion : Form
+    {
+        public InicioSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            var usuarioActual = new { usuario = this.txtUsuario.Text, contraseña = this.txtPass.Text};
+
+
+            if (this.txtUsuario.Text == "admin" && this.txtPass.Text == "admin")
+            {
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o contraseña incorrectos", "Login"
+                    , MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void lnkOlvidaPass_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Es Ud. un usuario muy descuidado, haga memoria", "Olvidé mi contraseña",
+        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+        }
+    }
+}
