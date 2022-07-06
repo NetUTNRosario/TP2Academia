381d9d77c4d3a6c45b695615e2365671c14563e8
MartinLambrecht <69395872+MartinLambrecht@users.noreply.github.com>
2022-07-06 15:40:03 -0300

    Creacion winform ABM-Modulos

---
+++ UI.Desktop/Modulos.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+
+namespace UI.Desktop
+{
+    public partial class Modulos : Form
+    {
+        public Modulos()
+        {
+            InitializeComponent();
+
+            this.dgvModulos.ReadOnly = true;
+
+            this.dgvModulos.AutoGenerateColumns = false;
+            this.dgvModulos.AllowUserToAddRows = false;
+            this.dgvModulos.AllowUserToDeleteRows = false;
+            this.dgvModulos.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            this.dgvModulos.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+
+            AddTextColumn("id", "ID", "ID");
+            AddTextColumn("descripcion", "Descripcion", "Descripcion");
+
+        }
+
+        public void Listar()
+        {
+            ModulosLogic el = new ModulosLogic();
+            this.dgvModulos.DataSource = el.GetAll();
+        }
+
+        private void AddTextColumn (string name, string headerText, string dataPropertyName)
+        {
+            DataGridViewTextBoxColumn newColumn = new DataGridViewTextBoxColumn();
+            newColumn.Name = name;
+            newColumn.HeaderText = headerText;
+            newColumn.DataPropertyName = dataPropertyName;
+
+            this.dgvModulos.Columns.Add(newColumn);
+        }
+
+        private void Modulos_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tsbNuevo_Click(object sender, EventArgs e)
+        {
+            ModuloDesktop especialidadDesktop= new ModuloDesktop( ApplicationForm.ModoForm.Alta);
+            especialidadDesktop.ShowDialog();
+            this.Listar();
+        }
+
+        private void tsbEliminar_Click(object sender, EventArgs e)
+        {
+            int ID = ((Business.Entities.Modulo)this.dgvModulos.SelectedRows[0].DataBoundItem).ID;
+
+            ModuloDesktop especialidadDesktop = new ModuloDesktop(ID,ApplicationForm.ModoForm.Baja);
+            especialidadDesktop.ShowDialog();
+            this.Listar();
+        }
+
+        private void tsbEditar_Click(object sender, EventArgs e)
+        {
+            int ID = ((Business.Entities.Modulo)this.dgvModulos.SelectedRows[0].DataBoundItem).ID;
+
+            ModuloDesktop especialidadDesktop = new ModuloDesktop(ID, ApplicationForm.ModoForm.Modificacion);
+            especialidadDesktop.ShowDialog();
+            this.Listar();
+        }
+    }
+}
---
+++ UI.Desktop/ModulosDesktop.cs
@@ -0,0 +1,136 @@
+﻿using Business.Entities;
+using Business.Logic;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace UI.Desktop
+{
+    public partial class ModuloDesktop : ApplicationForm
+    {
+
+        private Modulo ModuloActual;
+
+        public ModuloDesktop()
+        {
+            InitializeComponent();
+        }
+
+
+        public ModuloDesktop(ModoForm modo) : this()
+        {
+            this.modo = modo;
+
+            MapearDeDatos();
+        }
+
+        public ModuloDesktop(int ID, ModoForm modo) : this()
+        {
+            this.modo = modo;
+            ModulosLogic el = new ModulosLogic();
+            this.ModuloActual = el.GetOne(ID);
+            MapearDeDatos();
+        }
+
+        private new void MapearDeDatos()
+        {
+            if (modo == ModoForm.Alta)
+            {
+                this.txtID.Text = "";//Cuando hace el save lo genera automaticamente autoincremental
+            }
+            else
+            {
+                this.txtID.Text = this.ModuloActual.ID.ToString();
+                this.txtDescripcion.Text = this.ModuloActual.Descripcion;
+            }
+
+            switch (modo)
+            {
+                case ModoForm.Alta:
+                    this.Text = "Alta";
+                    this.btnAceptar.Text = "Guardar";
+                    break;
+
+                case ModoForm.Baja:
+                    this.Text = "Baja";
+                    this.btnAceptar.Text = "Eliminar";
+
+                    this.txtDescripcion.ReadOnly = true;
+                    this.txtDescripcion.TabStop = false;
+                    break;
+
+                case ModoForm.Modificacion:
+                    this.Text = "Modificación";
+                    this.btnAceptar.Text = "Guardar";
+                    break;
+
+                case ModoForm.Consulta:
+                    this.Text = "Consulta";
+                    this.btnAceptar.Text = "Aceptar";
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private new void MapearADatos()
+        {
+            if (modo == ModoForm.Alta)
+            {
+                this.ModuloActual = new Modulo();
+            }
+            this.ModuloActual.Descripcion = this.txtDescripcion.Text.Trim();
+            this.ModuloActual.State = (BusinessEntity.States)(int)modo;
+        }
+
+        private new void GuardarCambios()
+        {
+            MapearADatos();
+            ModulosLogic el = new ModulosLogic();
+            el.Save(ModuloActual);
+        }
+
+        private new bool Validar()
+        {
+            bool descripcionVal = ValidarCampoVacio(txtDescripcion, erpDescripcion, "La descripcion no puede estar vacia.");
+
+            bool isOK = descripcionVal;
+
+            if (!isOK) {
+                MessageBox.Show("Hay campos incorrectos, por favor verifique.","Campos incorrectos",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+            return isOK;
+        }
+
+        #region ValidacionesPersonalizadas
+
+        private bool ValidarCampoVacio(TextBox txtActual,ErrorProvider erpActual, string mensajeError)
+        {
+            if (String.IsNullOrEmpty(txtActual.Text.Trim()))
+            {
+                erpActual.SetError(txtActual, mensajeError);
+                return false;
+            }
+            else
+            {
+                erpActual.Clear();
+                return true;
+            }
+        }
+        #endregion
+
+
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            bool ok = Validar();
+            if (ok) { GuardarCambios(); this.Close(); }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
